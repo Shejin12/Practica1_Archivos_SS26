9409d3eab5958a90854e541041cba90964f45a2a
Sergio Rodriguez <sergiorodriguez202131443@cunoc.edu.gt>
2026-08-11 16:07:45 -0600

    frames

--- CentroMedico/src/main/java/com/mycompany/centromedico/CentroMedico.java
+++ CentroMedico/src/main/java/com/mycompany/centromedico/CentroMedico.java
@@ -4,14 +4,18 @@
 
 package com.mycompany.centromedico;
 
+import com.mycompany.centromedico.manejo.Inicio;
+
 /**
  *
  * @author chejo
  */
 public class CentroMedico {
+    public static Inicio inicio = new Inicio();
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        inicio.setVisible(true);
         
     }
 }
